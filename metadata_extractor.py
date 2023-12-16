d95b1db3b19bf8b3c98d66a287f24a28b20f73b0
JaffaShah <126200065+theshahyusuf@users.noreply.github.com>
2023-12-15 17:07:31 -0800

    Update metadata_extractor.py

--- metadata_extractor.py
+++ metadata_extractor.py
@@ -5,20 +5,24 @@ from PyPDF2 import PdfReader
 from docx import Document
 import datetime
 
+# Extract metadata from an image file
 def extract_image_metadata(file_path):
     with Image.open(file_path) as img:
-        metadata = img.info
+        metadata = img.info  # Retrieve metadata using PIL's info attribute
     return metadata
 
+# Extract metadata from a PDF file
 def extract_pdf_metadata(file_path):
-    with open(file_path, 'rb') as f:
-        pdf = PdfReader(f)  
-        metadata = pdf.metadata
+    with open(file_path, 'rb') as f:  # Open file in binary read mode
+        pdf = PdfReader(f)  # Read PDF file
+        metadata = pdf.metadata  # Extract metadata from PDF
     return metadata
 
+# Extract metadata from a Word document
 def extract_docx_metadata(file_path):
-    doc = Document(file_path)
-    core_properties = doc.core_properties
+    doc = Document(file_path)  # Open DOCX file
+    core_properties = doc.core_properties  # Access document properties
+    # Extract various metadata fields
     metadata = {
         'author': core_properties.author,
         'title': core_properties.title,
@@ -29,30 +33,40 @@ def extract_docx_metadata(file_path):
     }
     return metadata
 
+# Format metadata key for clearer output
 def format_metadata_key(key):
-    key = key.replace('/', '')  # Remove leading slash
+    key = key.replace('/', '')  # Remove leading slash for cleaner display
+    # Rename specific keys for more clarity
     if key == 'Creator':
         key = 'Software Used to Create File'
     elif key == 'Producer':
         key = 'Conversion Software'
     return key
 
+# Analyze the extracted metadata
 def analyze_metadata(metadata):
-    analysis = {'empty_fields': [], 'timestamp_issues': []}
-    # ... Add analysis logic here
+    analysis = {
+        'empty_fields': [],  # Placeholder for fields that are empty
+        'timestamp_issues': []  # Placeholder for timestamp related issues
+    }
+    # Analysis logic will be added here
     return analysis
 
+# Generate a report based on extracted and analyzed metadata
 def generate_report(metadata, analysis, report_path):
     with open(report_path, 'w') as file:
         file.write("Extracted Metadata:\n")
+        # Format and write metadata to the report
         for key, value in metadata.items():
             formatted_key = format_metadata_key(key)
             file.write(f"{formatted_key}: {value}\n")
         file.write("\nMetadata Analysis:\n")
+        # Write analysis results to the report
         for key, value in analysis.items():
             formatted_key = format_metadata_key(key)
             file.write(f"{formatted_key}: {value}\n")
 
+# Main function to run the script
 def main():
     if len(sys.argv) != 2:
         print("Usage: python metadata_extractor.py <file_path>")
@@ -64,6 +78,7 @@ def main():
         print("File does not exist.")
         sys.exit(1)
 
+    # Determine file type and extract metadata
     if file_path.lower().endswith(('.png', '.jpg', '.jpeg')):
         metadata = extract_image_metadata(file_path)
     elif file_path.lower().endswith('.pdf'):
